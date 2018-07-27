50153b264b3f1967b46bae5e9e8399b3b92436c2
unknown <roman.bruns@mail.ru>
2018-07-27 19:18:45 +0300

    Menu button, js & rotate

---
+++ dist/js/main.js
@@ -0,0 +1,40 @@
+/* Select DOM items */
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const menuBtnLine = document.querySelectorAll(".menu-btn__line");
+const navItems = document.querySelectorAll(".menu-nav-item");
+
+/* Set Initial State Of Menu */
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menu.classList.add("menu_show");
+    menuNav.classList.add("menu-nav_show");
+
+    menuBtn.classList.add("menu-btn_close");
+    menuBranding.classList.add("menu-branding_close");
+
+    menuBtnLine.forEach(item => item.classList.add("menu-btn__line_close"));
+    navItems.forEach(item => item.classList.add("menu-nav-item_show"));
+
+    /* Set Menu State */
+    showMenu = true;
+  } else {
+    menu.classList.remove("menu_show");
+    menuNav.classList.remove("menu-nav_show");
+
+    menuBtn.classList.remove("menu-btn_close");
+    menuBranding.classList.remove("menu-branding_close");
+
+    menuBtnLine.forEach(item => item.classList.remove("menu-btn__line_close"));
+    navItems.forEach(item => item.classList.remove("menu-nav-item_show"));
+
+    /* Set Menu State */
+    showMenu = false;
+  }
+}
